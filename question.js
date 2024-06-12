f31d35631c841e292682862c71708372c1d00f75
Gulshan <gk6715961@gmail.com>
2024-06-12 17:01:20 +0530

    initial

---
+++ question.js
@@ -0,0 +1,148 @@
+let questions = [
+    {
+        numb: 1,
+        question:"What is the main ingredient of famous Hyderabadi dish “Haleem”?",
+        answer:"D.Mutton",
+        options: [
+            "A.kekda",
+            "B.Fish",
+            "C.Lobster",
+            "D.Mutton"
+
+        ]
+
+    },
+
+
+{
+    numb: 2,
+    question:"How many times has India won the Cricket ODI World Cup?",
+    answer:"A.2",
+    options: [
+        "A.2",
+        "B.3",
+        "C.1",
+         "D.4"
+
+
+    ]
+
+},
+
+
+
+{
+    numb: 3,
+    question:"What was Arijit Singh's Bollywood Debut Songs ?",
+    answer:"B.Phir Mohabbat",
+    options: [
+         "A.Tum Hi ho",
+        "B.Phir Mohabbat",
+        "C.Duaa",
+        "D.Meri Maa"
+    ]
+
+},
+
+
+
+{
+    numb: 4,
+    question:"Which of these is a widely used web browser of Microsoft?",
+    answer:"C.IE",
+    options: [
+        "A.Firefox",
+        "B.Chrome",
+        "C.IE",
+        "D.Safari"
+
+    ]
+
+},
+
+{
+    numb: 5,
+    question:"Who wrote the novel 1984?",
+    answer:"A.George Orwell",
+    options: [
+        "A.George Orwell",
+        "B.J.K. Rowling",
+        "C.F. Scott Fitzgerald",
+        "D.Ernest Hemingway"
+
+    ]
+
+},
+
+{
+    numb: 6,
+    question:"What is the tallest mountain in the world?",
+    answer:"B.Mount Everest",
+    options: [
+        "A.K2",
+        "B.Mount Everest",
+        "C.Mount Kilimanjaro",
+        "D.Denali"
+
+    ]
+
+},
+
+{
+    numb: 7,
+    question:"What is the capital city of Australia ?",
+    answer:"C.Canberra",
+    options: [
+        "A.Sydney",
+        "B.Melbourne",
+        "C.Canberra",
+        "D.Brisbane"
+
+    ]
+
+},
+
+{
+    numb: 8,
+    question:"What is the chemical symbol for Gold? ?",
+    answer:"D.Au",
+    options: [
+        "A.Gd",
+        "B.Go",
+        "C.Ag",
+        "D.Au"
+
+    ]
+
+},
+
+{
+    numb: 9,
+    question:"In what year was the first iPhone released ?",
+    answer:"B.2007",
+    options: [
+        "A.2005",
+        "B.2007",
+        "C.2008",
+        "D.2010"
+
+    ]
+
+},
+
+
+
+{
+    numb: 10,
+    question:"Who Won the first World Cup in Football ?",
+    answer:"C.Uruguay",
+    options: [
+        "A.Russia",
+        "B.Brazil",
+        "C.Uruguay",
+        "D.England"
+    ]
+
+} 
+];
+
